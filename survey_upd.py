d6768c5a91096ce531d385d31bf35fc178f647d1
Manish Narayan <31181534+ManishNarayan@users.noreply.github.com>
2019-09-05 19:37:32 +0530

    Add files via upload

--- survey_upd.py
+++ survey_upd.py
@@ -13,16 +13,16 @@ for i in range(len(survey)):
     id = survey['id'][i]
     #print(v_link)
     result = detect.object_detection_function(v_link,id)
-    #result=[1,2,3,4,5,6,7,8]
-    survey.at[i,10] = result[0]
     
-    survey.at[i,11] = result[1]
-    survey.at[i,12] = result[2]
-    survey.at[i,13] = result[3]
-    survey.at[i,14] = result[4]
-    survey.at[i,15] = result[5]
-    survey.at[i,16] = result[6]
-    survey.at[i,17] = result[7]
+    survey.iat[i,9] = result[0]
+    survey.iat[i,10] = result[1]
+    survey.iat[i,11] = result[2]
+    survey.iat[i,12] = result[3]
+    survey.iat[i,13] = result[4]
+    survey.iat[i,14] = result[5]
+    survey.iat[i,15] = result[6]
+    survey.iat[i,16] = result[7]
+survey_csv = survey.to_csv(r'C:\Users\HP\Desktop\Intrn\vehicle_counting_tensorflow\export.csv',index = None , header = True)
     
 
 
